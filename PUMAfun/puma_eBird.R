43aced46a22fe68604be84923e53af64b21a2501
Katie Bird <61510844+veerydog@users.noreply.github.com>
2020-02-26 15:45:56 -0500

    Update puma_ebird.R
    Added additional comments and cleaned up existing comments.

--- PUMAfun/puma_eBird.R
+++ PUMAfun/puma_eBird.R
@@ -1,11 +1,17 @@
-#########################################
-# Filtering and Processin PUMA eBird Data
-# Katie Bird
-# 13 November 2019
-#########################################
+#########################################################################################################
+###                              Filtering and Processing PUMA eBird Data                             ###
+###                                             Katie Bird                                            ###
+###                                       Written: 13 November 2019                                   ###
+###                                          Edited: 26 Feb 2020                                      ###
+#########################################################################################################
 
-######   Step 1: Install Libraries   ####
-{#install.packages(c("auk", raster", "tidyvserse", "sf", "rnaturalearth", "rnaturalearthhires", "tigris", "tigris", "viridisLite", "leaflet"))
+########################################################################################
+###                         Step 1: Install Libraries                                ###
+
+#Run loop to install all necessary libraries.
+{#install.packages(c("auk", raster", "tidyvserse", "sf",
+    #"rnaturalearth", "rnaturalearthhires", "tigris", "tigris",
+    #"viridisLite", "leaflet"))
     library(auk)
     library(raster)
     library(tidyverse)
@@ -17,11 +23,13 @@
     library(viridisLite)
     library(leaflet)
     }
-#########################################
 
-######    Step 2: Set up paths      #####
+########################################################################################
+###                           Step 2: Set up paths                                   ###
 
-# path to ebird data txt file
+#Run each line of code to set up necessary paths & objects.
+
+# path to ebird data txt file, i.e. ebird data directory
 ebd_dir <- "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/"
 
 # ebd
@@ -33,7 +41,7 @@ f_in_ebd <- file.path(ebd_dir, "ebd_US_purmar_relSep-2019.txt")
 # create an object referencing this file
 auk_ebd(file = f_in_ebd)
 
-#Now let's apply the filters we want.
+#Now let's apply the filters we want. I filtered by state and the roosting season.
 ebd_filters <- auk_ebd(f_in_ebd) %>%
     auk_state(c("US-DE", "US-NJ", "US-MD")) %>%
     auk_date(c("*-07-01", "*-09-30")) %>%
@@ -44,45 +52,50 @@ ebd_filters
 f_out_ebd <- "ebd_puma_de-nj-md_fallmig.txt"
 ebd_filtered <- auk_filter(ebd_filters, file = f_out_ebd, overwrite = TRUE)
 
-ebd <- read_ebd(f_out_ebd)
-glimpse(ebd)
+ebd <- read_ebd(f_out_ebd) #redefines the ebird data object
+glimpse(ebd)               #shows you what the data basically looks like.
 
 ebd$observation_count <- as.numeric(ebd$observation_count)
-roosts <- subset(ebd, observation_count>100)
-roosts_NJ <- subset(roosts, roosts$state == "New Jersey")
-roosts_MD <- subset(roosts, roosts$state == "Maryland")
-roosts_DE <- subset(roosts, roosts$state == "Delaware")
+roosts <- subset(ebd, observation_count>100) #creates a subset of all the observations. Observations over 100 martins = a "roost"
+roosts_NJ <- subset(roosts, roosts$state == "New Jersey") #defines a new object for New Jersey roosts
+roosts_MD <- subset(roosts, roosts$state == "Maryland") #defines a new object for Maryland roosts
+roosts_DE <- subset(roosts, roosts$state == "Delaware") #defines a new object for Delaware roosts
 
+#The next three lines write out .csv files for roost data in each state.
 write.csv(roosts_DE, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_de.csv", row.names = FALSE)
-
 write.csv(roosts_MD, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_md.csv", row.names = FALSE)
-
 write.csv(roosts_NJ, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_nj.csv", row.names = FALSE)
 
-#########################################
-###          Plot a Static Map        ###
-###      Don't run this as a loop!    ###
-#########################################
-{#Now that the data is in R, we can make a map of it.
+########################################################################################
+###                               Plot a Static Map                                  ###
+###                                                                                  ###
+########################################################################################
+#Now that the data is in R, we can make a map of it.
+
     # convert ebd to spatial object
     ebd_sf <- roosts %>% 
         select(common_name, latitude, longitude) %>% 
         st_as_sf(coords = c("longitude", "latitude"), crs = 4326)
+
     # get state boundaries using rnaturalearth package
     states <- ne_states(iso_a2 = c("US", "CA"), returnclass = "sf")
     de <- filter(states, postal == "DE") %>% 
         st_geometry()
-    # map
+
+    # map, set background color to blue
     par(mar = c(0, 0, 0, 0), bg = "skyblue")
+
     # set plot extent
     plot(de, col = NA)
+
     # add state boundaries
     plot(states %>% st_geometry(), col = "grey40", border = "white", add = TRUE)
     plot(de, col = "grey20", border = "white", add = TRUE)
+
     # ebird data
     plot(ebd_sf %>% filter(common_name == "Purple Martin"), 
          col = "#4daf4a99", pch = 19, cex = 0.75, add = TRUE)
-    }
+    
 
 ##################################################################    
 ######    Make an interactive map of roosts for MD, NJ, DE.    ###
@@ -108,8 +121,10 @@ getColor <- function(roosts_NJ)
     sapply(roosts_NJ$observation_count, function(observation_count) 
         if(observation_count >= 1000) {"green"
         } else if(observation_count>=101 & observation_count<=999) {
-            "orange"})
+            "orange"}) #I'm not sure if I ever got this to actually work or not.
+                       #Supposedly you can segregate and color code data this way
 
+#Assign a cool symbol to be your map marker.
 icons <- awesomeIcons(
     icon = 'ios-close',
     iconColor = 'black',
@@ -138,7 +153,7 @@ icons <- awesomeIcons(
 leaflet(roosts_MD) %>% addTiles() %>%
     addAwesomeMarkers(roosts_MD$longitude, roosts_MD$latitude, icon=icons, label=~as.character(observation_count))
 
-####################################################################
+####################################################################################################################
 
 getColor <- function(roosts_DE) 
     sapply(roosts_DE$observation_count, function(observation_count) 
@@ -156,8 +171,9 @@ icons <- awesomeIcons(
 leaflet(roosts_DE) %>% addTiles() %>%
     addAwesomeMarkers(roosts_DE$longitude, roosts_DE$latitude, icon=icons, label=~as.character(observation_count))
 
-###################################################################################
-
+#####################################################################################################################
+########### Here I reused the code from above to filter the ebird data again for the south east. ####################
+           
 #Now let's apply the filters we want.
 ebd_filters <- auk_ebd(f_in_ebd) %>%
     auk_state(c("US-VA", "US-NC", "US-SC")) %>%
@@ -178,14 +194,14 @@ roosts_VA <- subset(roosts, roosts$state == "Virginia")
 roosts_NC <- subset(roosts, roosts$state == "North Carolina")
 roosts_SC <- subset(roosts, roosts$state == "South Carolina")
 
+#Write out .csv files for filtered ebird data.           
 write.csv(roosts_VA, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_va.csv", row.names = FALSE)
-
 write.csv(roosts_NC, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_nc.csv", row.names = FALSE)
-
 write.csv(roosts_SC, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_sc.csv", row.names = FALSE)
 
-######################################################################################
-
+#####################################################################################################################
+########### Here I reused the code from above to filter the ebird data again for the north east. ####################
+           
 #Now let's apply the filters we want.
 ebd_filters <- auk_ebd(f_in_ebd) %>%
     auk_state(c("US-PA", "US-NY", "US-CT")) %>%
@@ -206,8 +222,7 @@ roosts_PA <- subset(roosts, roosts$state == "Pennsylvania")
 roosts_NY <- subset(roosts, roosts$state == "New York")
 roosts_CT <- subset(roosts, roosts$state == "Connecticut")
 
+#Write out .csv files for filtered ebird data.  
 write.csv(roosts_PA, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_pa.csv", row.names = FALSE)
-
 write.csv(roosts_NY, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_ny.csv", row.names = FALSE)
-
 write.csv(roosts_CT, "C:/Users/radar/Desktop/Projects/PurpleMartin_Project/ebird/roosts_ct.csv", row.names = FALSE)
